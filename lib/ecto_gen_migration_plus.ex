63a2f0b86486ec26382d514d56f6fc00ea491408
Michael Johnston <lastobelus@mac.com>
2021-04-15 21:28:27 -0700

    initial commit: mix new ecto_gen_migration_plus

---
+++ lib/ecto_gen_migration_plus.ex
@@ -0,0 +1,18 @@
+defmodule EctoGenMigrationPlus do
+  @moduledoc """
+  Documentation for `EctoGenMigrationPlus`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoGenMigrationPlus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_gen_migration_plus_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoGenMigrationPlusTest do
+  use ExUnit.Case
+  doctest EctoGenMigrationPlus
+
+  test "greets the world" do
+    assert EctoGenMigrationPlus.hello() == :world
+  end
+end
